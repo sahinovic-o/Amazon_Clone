411ecb6df500f96a56cfe62e6cbabd6c543731f8
Omar Šahinović <sahinovicomar@gmail.com>
2022-11-28 14:18:16 +0100

    amazon clone

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const { application } = require("express");
+const express = require("express");
+const mongoose = require("mongoose");
+const adminRouter = require("./routes/admin");
+
+// Import grom other files
+const authRouter = require("./routes/auth");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
+
+// init
+const PORT = 3000;
+const app = express();
+const DB = "mongodb+srv://omar:learning@cluster0.cvs6t7k.mongodb.net/?retryWrites=true&w=majority";
+
+// middleware
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+
+//connections
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`connected at port ${PORT}`);
+  });
